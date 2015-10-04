6cd8855d05b6fb63819931f074b79a472db4e2af
Tsotnep <tsotnep@gmail.com>
2015-10-04 19:59:36 +0300

    circuit with delay, with its testbench

---
+++ arm_verilog/tb_arm_verilog.v
@@ -0,0 +1,15 @@
+//simulation
+
+module stimcrct;
+reg A, B, C;
+wire x, y;
+circuit_with_delay cwd(A, B, C, x, y);
+
+initial
+	begin
+		A=1'b0; B=1'b0; C=1'b0;
+		#100
+		A=1'b1; B=1'b1; C=1'b1;
+		#100 $finish;
+	end
+endmodule
