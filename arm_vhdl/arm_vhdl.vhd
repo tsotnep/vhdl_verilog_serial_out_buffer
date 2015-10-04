dc376ccd18f943b7246d5541a21bb52843d3db8a
Tsotnep <tsotnep@gmail.com>
2015-10-04 12:53:18 +0300

    comments added, code optimized

--- arm_vhdl/arm_vhdl.vhd
+++ arm_vhdl/arm_vhdl.vhd
@@ -1,21 +1,25 @@
+--	
+	
+	-- circuit looks like it's a serial communication module, where we are providing
+	-- clock and data
+
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---bitNumber          <= STD_LOGIC_VECTOR(unsigned(0));
---bitNumber <= STD_LOGIC_VECTOR(unsigned(bitNumber) + 1);
---OutD      <= concatenatedInputs(integer(unsigned(bitNumber)));
---sizeCount   : INTEGER := 5
-
 entity arm_vhdl is
 	generic(
-		sizeA       : INTEGER := 7;
-		sizeD       : INTEGER := 8;
-		sizeOfpause : INTEGER := 1
+		sizeA      : INTEGER := 7;      --size of first input 
+		sizeD      : INTEGER := 8;      --size of second input 
+		sizeHeader : INTEGER := 1;      --size of header, LeftMost and RightMost bits, in our case we send only : '0'
+		sizeZPause : INTEGER := 1       --size of pause/delay after sending A and B. in our case we send only : 'Z'
 	);
 	port(
 		OutD    : out STD_LOGIC;
 		OutC    : out STD_LOGIC;
+
 		A       : in  STD_LOGIC_VECTOR(sizeA - 1 downto 0);
 		D       : in  STD_LOGIC_VECTOR(sizeD - 1 downto 0);
 		Go      : in  STD_LOGIC;
@@ -25,20 +29,17 @@ entity arm_vhdl is
 end entity arm_vhdl;
 
 architecture RTL of arm_vhdl is
-	constant zPause           : STD_LOGIC_VECTOR(sizeOfpause - 1 downto 0)                 := (others => 'Z');
-	constant ones             : STD_LOGIC_VECTOR(0 to sizeA + sizeD + 2 * sizeOfpause + 1) := (others => '1');
-	constant endStringMatch   : STD_LOGIC_VECTOR(0 to sizeA + sizeD + 2 * sizeOfpause + 1) := (0 => 'Z', others => '1');
-	--	signal bitNumber          : STD_LOGIC_VECTOR(sizeCount - 1 downto 0);
-	signal concatenatedInputs : STD_LOGIC_VECTOR(sizeA + sizeD + 2 * sizeOfpause + 1 downto 0);
-	--it should be  ..size-1, but, +2 for '0's in the start & end 
+	constant outputSize : integer := sizeA + sizeD + 2 * sizeZPause + 2 * sizeHeader; --this is number that identifies how large is a vector that will be sended
 
-	signal ended        : STD_LOGIC := '0';
-	signal endedDelayed : STD_LOGIC := '0';
+	constant zPause       : STD_LOGIC_VECTOR(sizeZPause - 1 downto 0) := (others => 'Z'); --this is a High Impedance after sending input
+	constant header       : STD_LOGIC_VECTOR(sizeHeader - 1 downto 0) := (others => '0'); --this part is inserted in most Left and Right sides of concatenated string
+	constant sendingEnded : STD_LOGIC_VECTOR(outputSize - 1 downto 0) := (others => '1'); --when concatenated vector becomes all ones, that means sending has ended
+
+	signal concatenatedInputs : STD_LOGIC_VECTOR(outputSize - 1 downto 0) := (others => '1');
+	signal becomeIdle         : STD_LOGIC                                 := '0';
 begin
-	-- circuit looks like it's a serial communication module, where we are providing
-	-- clock and data
 
-	OutC <= '1' when ended = '1'
+	OutC <= '1' when becomeIdle = '1'
 		else '1' when reset_n = '0'
 		else clk_in;
 
@@ -46,21 +47,21 @@ begin
 	begin
 		if reset_n = '0' then
 			OutD               <= '1';
-			ended              <= '1';
-			concatenatedInputs <= (others => '1');
+			becomeIdle         <= '1';
+			concatenatedInputs <= (others => '1'); --we reset it on '1' because when sending is ended, output is '1'
 		elsif Go = '1' then
-			concatenatedInputs <= '0' & A & zPause & D & zPause & '0';
+			concatenatedInputs <= header & A & zPause & D & zPause & header; --this line concatenates inputs, pauses, and headers
 		elsif rising_edge(clk_in) then
-			OutD               <= concatenatedInputs(sizeA + sizeD + 2 * sizeOfpause + 1); --outputs Left Most bit
-			concatenatedInputs <= concatenatedInputs(sizeA + sizeD + 2 * sizeOfpause downto 0) & '1';
-			--shifts left, we add '1' on the right, instead of '0', because of specification from diagram
+			OutD <= concatenatedInputs(outputSize - 1); --this output, that gets Left Most bit of the concatenated vector. that was required.
 
-			if concatenatedInputs = ones then
-				ended <= '1';
+			concatenatedInputs <= concatenatedInputs(outputSize - 2 downto 0) & '1'; --this line shifts the concatenated vector to the left, so that current LM bit that was outputed on this clock cycle, will be lost and next bit will become LM
+
+			if concatenatedInputs = sendingEnded then
+				becomeIdle <= '1';      --this signal is '1' when sending finished or has not started, we use it for controlling "OutC" 
 			else
-				ended <= '0';
+				becomeIdle <= '0';
 			end if;
-			endedDelayed <= ended;
+
 		end if;
 	end process;
 end architecture RTL;
--- arm_vhdl/tb_arm_vhdl.vhd
+++ arm_vhdl/tb_arm_vhdl.vhd
@@ -4,15 +4,19 @@ use ieee.numeric_std.all;
 
 entity tb_arm_vhdl is
 	generic(
-		sizeA       : INTEGER := 7;
-		sizeD       : INTEGER := 8;
-		sizeOfpause : INTEGER := 1
+		sizeA : INTEGER := 7;
+		sizeD : INTEGER := 8;
+		sizeHeader : INTEGER := 1;
+		sizeZPause : INTEGER := 1
 	);
 end entity tb_arm_vhdl;
 
 architecture RTL of tb_arm_vhdl is
+	--outputs
 	signal OutD    : STD_LOGIC;
 	signal OutC    : STD_LOGIC;
+	
+	--inputs
 	signal A       : STD_LOGIC_VECTOR(sizeA - 1 downto 0);
 	signal D       : STD_LOGIC_VECTOR(sizeD - 1 downto 0);
 	signal Go      : STD_LOGIC;
@@ -42,16 +46,18 @@ begin
 
 	stimul_p : process
 	begin
-		reset_n <= '0';
-		wait for t;
-		reset_n <= '1';
-		wait for 35 ns;
-		wait for t;
-		A  <= "0110100";
-		D  <= "10111001";
-		Go <= '1';
-		wait for t;
-		Go <= '0';
+		reset_n <= '0';	wait for t;	reset_n <= '1'; wait for 35 ns; --reset and wait for a while
+	
+		A  <= "1111111"; D  <= "11111111"; 	Go <= '1'; 	wait for t;  Go <= '0'; --test 1
+		wait for t*(sizeA + sizeD + sizeHeader + sizeZPause + 5);
+		
+		A  <= "1110111"; D  <= "11100111"; 	Go <= '1'; 	wait for t;  Go <= '0'; --test 2
+		wait for t*(sizeA + sizeD + sizeHeader + sizeZPause + 5);
+		
+		A  <= "0110100"; D  <= "10111001"; 	Go <= '1'; 	wait for t;  Go <= '0'; --test 3
+		wait for t*(sizeA + sizeD + sizeHeader + sizeZPause + 5); 
+		
+		
 		wait;
 	end process stimul_p;
 
