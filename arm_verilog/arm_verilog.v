bedbb65c1de6ac1185479a92d69c43e4444fb048
Tsotnep <tsotnep@gmail.com>
2015-10-06 18:22:21 +0300

    OutC corrected, now it outputs '1' when idle

--- arm_verilog/arm_verilog.v
+++ arm_verilog/arm_verilog.v
@@ -1,12 +1,19 @@
 `timescale 1ns/1ps
-
-module arm_verilog (OutD, OutC, D, A, Go, clk_in, reset_n); //when reset_n = 0 we reset everything. 
-	//parameters-generics
+module arm_verilog (
+	OutD,		//output OutD, this one bit serial output gives out data: A and then B. transmission starts and ends with '0', after each part: A,B, it gives out 'Z' signal for 1 clock cycle 
+	OutC, 		//output OutC, it's output clock, that is probably used by receiver (which is probably in different domain) to clock this output of this system in right frequency, so that it will get correct data
+	D, 			//input D, 8 bits, [MSB:LSB]
+	A, 			//input A, 7 bits, [MSB:LSB]
+	Go, 		//when go makes positive pulse, we save inputs and start to output them on next clock cycle
+	clk_in, 	//clock input
+	reset_n		//when reset_n = 0 we reset everything. 
+); 
+//parameters-generics
 	parameter sizeA = 7;
 	parameter sizeD = 8;
 	parameter constAllBitOnes = {18{1'b1}};
 
-	//I/O
+//I/O
 	output OutD;
 	output OutC;
 	input [sizeA-1:0] A;
@@ -15,7 +22,7 @@ module arm_verilog (OutD, OutC, D, A, Go, clk_in, reset_n); //when reset_n = 0 w
 	input clk_in;
 	input reset_n;	
 	
-	//I/O types
+//I/O types
 	reg OutD;
 	reg OutC;
 	wire [sizeA-1:0] A;
@@ -24,11 +31,11 @@ module arm_verilog (OutD, OutC, D, A, Go, clk_in, reset_n); //when reset_n = 0 w
 	wire clk_in;
 	wire reset_n;
 	
-	//internal signals
-	reg [18:0] Concatenated_Inputs;
-	reg enable_OutC;
+//internal signals
+	reg [18:0] Concatenated_Inputs; //when Go signal arrives, this vector gets-saves '0'+A+'Z'+D+'Z'+'0' 
+	reg enable_OutC; //this is used to control OutC output
 	
-	//initial values, resetted
+//initial values, resetted
 	initial begin
 		OutD <= 1'b1;
 		OutC <= 1'b1;
@@ -36,39 +43,41 @@ module arm_verilog (OutD, OutC, D, A, Go, clk_in, reset_n); //when reset_n = 0 w
 		Concatenated_Inputs <= {19{1'b1}};
 	end
 	
-	//assigning, shifting and outputting concatenated vector of inputs
+//assigning, shifting and outputting concatenated vector of inputs
 	always @ (posedge clk_in or posedge reset_n)
 	begin : main_function
 		if (reset_n == 1'b0) begin
 			Concatenated_Inputs <= {19{1'b1}};
 			OutD <= 1'b1;
-		end else if (Go == 1'b1) begin
+		end 
+		else if (Go == 1'b1) begin
 			Concatenated_Inputs <= {1'b0, A, 1'bZ, D, 1'bZ, 1'b0};
-		end else fork
+		end 
+		else begin
 			Concatenated_Inputs <= {Concatenated_Inputs[17:0], 1'b1};
 			OutD <= Concatenated_Inputs[18];			
-		join
+		end
 	end
 	
-	//setting enable signal which controls when OutC will be active.
+//setting enable signal which controls when OutC will be active.
 	always @ (posedge clk_in or posedge reset_n)
 	begin : enabling_output_c
 		if (reset_n == 1'b0) begin
 			enable_OutC <= 1'b0;
-		end else if (Concatenated_Inputs[18-1:0] == constAllBitOnes) begin //on this pattern, we should set OutC to zero
+		end else if (Concatenated_Inputs[18-1:0] == constAllBitOnes) begin //on this pattern, we should reset OutC to zero. its "ending pattern"
 			enable_OutC <= 1'b0;
 		end else begin
 			enable_OutC <= 1'b1;
 		end
 	end
 	
-	//outputting OutC
-	always @(posedge clk_in or negedge clk_in or posedge reset_n)
+//outputting OutC
+	always @(clk_in or reset_n or enable_OutC)
 	begin : output_c
 		if (reset_n == 1'b0) begin
 			OutC <= 1'b1;
 		end else begin
-			OutC <= clk_in && enable_OutC;
+			OutC <= ~((~clk_in) && enable_OutC);
 		end	
 	end	
 endmodule 
--- arm_verilog/tb_arm_verilog.v
+++ arm_verilog/tb_arm_verilog.v
@@ -9,25 +9,32 @@ module tb_shift_register();
 	reg reset_n;	
 	
 initial begin
-	$display ("testing starts here");
-	reset_n = 0; Go = 0; clk_in = 1; D = 7'b00000000; A = 7'b0000000;  //reset
+
+//reset	
+	reset_n = 0; Go = 0; clk_in = 1; D = 7'b00000000; A = 7'b0000000;  
 	#10 reset_n = 1;
 	
+$display ("test pattern N1");
+//test pattern N1
 	#10 D = 8'b11111111; A = 7'b1111111;	Go = 1;
 	#10 D = 7'b00000000; A = 7'b0000000;	Go = 0;
-	
 	#(30*10)
-	#10 D = 8'b10011111; A = 7'b1000001;	Go = 1;
-	#10 D = 7'b00000000; A = 7'b0000000;	Go = 0;
+	
+//$display ("test pattern N2");	
+////test pattern N2
+//	#10 D = 8'b10011111; A = 7'b1000001;	Go = 1;
+//	#10 D = 7'b00000000; A = 7'b0000000;	Go = 0;
+//	#(30*10)
 	
 	#100 $finish;
 end 
 
-always begin
-	#5 clk_in = ~clk_in;
-end 
+//clock driver
+	always
+	begin 
+		#5 clk_in = ~clk_in;
+	end 
 
-
-
-arm_verilog armv_inst (OutD, OutC, D, A, Go, clk_in, reset_n);
+//instantiation of uut
+	arm_verilog armv_inst (OutD, OutC, D, A, Go, clk_in, reset_n);
 endmodule
