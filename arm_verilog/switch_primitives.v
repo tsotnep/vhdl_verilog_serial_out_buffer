863174d63bc4574aa0bc4130d8fc4e6b9b70b9fa
Tsotnep <tsotnep@gmail.com>
2015-10-06 19:49:19 +0300

    learning verilog files deleted

--- arm_verilog/switch_primitives.v
+++
@@ -1,9 +0,0 @@
-module switch_primitives();
-
-wire  net1, net2, net3;
-wire  net4, net5, net6;
-
-tranif0 my_gate1 (net1, net2, net3); //Bi-directional transistor (Low)
-rtranif1 my_gate2 (net4, net5, net6);
-
-endmodule
